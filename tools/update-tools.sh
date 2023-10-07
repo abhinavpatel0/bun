5241b661ab5f077209023e70f4d0f76e6e48f34e
abhinavpatel0 <121202898+abhinavpatel0@users.noreply.github.com>
2023-10-07 11:02:38 +0530

    Update update-tools.sh

--- tools/update-tools.sh
+++ tools/update-tools.sh
@@ -4,13 +4,14 @@ set -e
 cd tools/
 rm -rf zig/ zls/
 
-ZIG=$(wget --quiet --output-document=- https://ziglang.org/download/index.json | jq --raw-output '.master."x86_64-linux".tarball')
-echo installing latest zig
-wget --quiet --output-document=- $ZIG | tar Jx
+# Download the specific Zig version
+ZIG_URL="https://ziglang.org/builds/zig-linux-x86_64-0.12.0-dev.163+6780a6bbf.tar.xz"
+echo "Installing Zig version 0.12.0-dev.163+6780a6bbf"
+wget --quiet --output-document=- "$ZIG_URL" | tar Jx
 mv zig-linux-x86_64-* zig
-echo zig version $(./zig/zig version)
+echo "Zig version $(./zig/zig version)"
 
-echo installing latest zls - zig language server
+echo "Installing latest ZLS - Zig Language Server"
 git clone --quiet --recurse-submodules https://github.com/zigtools/zls
 cd zls
 set +e
